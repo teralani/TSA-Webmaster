e971411c4d1cbf937c288b02aa191a115fca8dba
s-athadani <1087428@lwsd.org>
2024-11-19 19:35:20 -0800

    car sort of moving along path. not fully working yet

--- app.js
+++ app.js
@@ -6,3 +6,28 @@ function showOptions() {
         buttons.classList.add("show_options");
     }
 }
+const road = document.getElementById('roadBG');
+
+const car = document.getElementById('car');
+const roadHeight = document.getElementById('line').getBoundingClientRect().height;
+const roadLength = road.getTotalLength();
+
+let progress = 0
+let prevangle = 90
+const k = 1 ;
+function animate(){
+    
+    progress = Math.pow(window.scrollY-(0.4*window.outerHeight),k)/(roadHeight);
+    console.log(progress*100);
+    const point = road.getPointAtLength(progress * roadLength);
+    const Dirpoint = road.getPointAtLength((progress+0.01) * roadLength);
+    let angle = Math.atan((Dirpoint.y-point.y)/(Dirpoint.x-point.x))*(180/Math.PI)+90;
+    if(Math.abs(angle-prevangle) > 90 && progress<0.5){
+        angle-=180
+    }
+    car.setAttribute('transform','translate(' + point.x + ', ' + point.y + ') rotate('+angle+')');
+    prevangle = angle
+    
+}
+window.addEventListener("scroll", () => animate(),{ passive: true });
+
