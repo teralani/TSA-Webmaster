c248c6845325edd65624e4cfdf7b702d0c42afff
teralani <>
2024-11-23 21:47:15 -0800

    ?

--- about.js
+++ about.js
@@ -1,11 +1,3 @@
-function showOptions() {
-    buttons = document.getElementById("nav_buttons");
-    if (buttons.classList.contains("show_options")){
-        buttons.classList.remove("show_options");
-    } else {
-        buttons.classList.add("show_options");
-    }
-}
 const road = document.getElementById('roadBG');
 
 const car = document.getElementById('car');
--- menu.js
+++ menu.js
@@ -1,15 +1,16 @@
 const wrapper = document.body.querySelectorAll(".wrapper");
 
 function parallax() {
-   let offset = window.scrollY;
    wrapper.forEach((el) => {
         switch(el){
             case wrapper[0]:
-                c = 0; break;
+                c = 150; break;
             case wrapper[1]:
-                c = 300; break;
+                c = 400; break;
             case wrapper[2]:
-                c = 100 ; break;   
+                c = 200 ; break;
+            case wrapper[3]:
+                c = 200 ; break;  
         }
         let o = window.scrollY - el.getBoundingClientRect().top - visualViewport.offsetTop;
         if(o >= 0) {
