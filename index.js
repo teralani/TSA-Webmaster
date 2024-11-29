16aa519e29c5c31bb49ce98fb610cf33411b9da7
Rishi Ruia <rishiru@outlook.com>
2024-11-28 16:18:48 -0800

    pushed nothing i think

--- index.js
+++ index.js
@@ -16,3 +16,14 @@ btn.forEach((el) => {
     el.style.transform = `translate(0,0)`;
   });
 });
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
