fb2fa40b45fa45b771d798522aba48be886a5855
Rishi Ruia <rishiru@outlook.com>
2024-12-07 23:13:49 -0800

    Yo aniketh the type writer breaks everything we might to have to kill it or make it single line

--- scrubbing.js
+++ scrubbing.js
@@ -3,19 +3,17 @@ const vid = section.querySelector('video')
 vid.pause()
 
 const scroll =()=> {
-    const distance = window.scrollY -  section.offsetTop
-    const total = section.clientHeight - window.innerHeight
-    
-    let percentage = distance / total
+    const distance = window.scrollY -  section.offsetTop;
+    const total = section.clientHeight - window.innerHeight ;
+    let percentage = distance / (total*1.8);
     percentage = Math.max(0, percentage)
-    percentage = Math.min(percentage,1 )
+    percentage = Math.min(percentage,1/1.3 )
     if(percentage != 1)
     if(vid.duration >0){
-        vid.currentTime = vid.duration*percentage
-        console.log("crack coaine")
+        vid.currentTime = vid.duration*percentage;
     }
     
 }
 
 scroll()
-window.addEventListener("scroll", scroll)
+window.addEventListener("scroll", scroll);
