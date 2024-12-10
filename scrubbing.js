3a0987588e08c686e5523b9f0b1e0ea3194127f8
Rishi Ruia <rishiru@outlook.com>
2024-12-09 17:46:49 -0800

    homepage done pc version

--- scrubbing.js
+++ scrubbing.js
@@ -5,9 +5,9 @@ vid.pause()
 const scroll =()=> {
     const distance = window.scrollY -  section.offsetTop;
     const total = section.clientHeight - window.innerHeight ;
-    let percentage = distance / (total);
+    let percentage = distance / (total*2);
     percentage = Math.max(0, percentage)
-    percentage = Math.min(percentage,1 )
+    percentage = Math.min(percentage,1/2 )
     if(percentage != 1)
     if(vid.duration >0){
         vid.currentTime = vid.duration*percentage;
